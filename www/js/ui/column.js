4fdc331fec3cbc1ff6197caee586f7bff7be9988
ares <ares@MacBook-de-ares.local>
2015-06-05 14:54:13 -0500

    piezas

--- www/js/ui/column.js
+++ www/js/ui/column.js
@@ -4,12 +4,22 @@ uiClasses.factory('Column', function(Piece){
 	=========================*/
 	function Column(properties){
 		this.initialize();
+		properties.numberOfPieces;
 		this.piece1 = new Piece({x: 0, y: 0, scale: properties.scale, pieceName: properties.pieceName});
 		this.piece2 = new Piece({x: 0, y: 140 * properties.scale, scale: properties.scale, pieceName: properties.pieceName});
 		this.piece3 = new Piece({x: 0, y: 275 * properties.scale, scale: properties.scale, pieceName: properties.pieceName});
 		this.piece4 = new Piece({x: 0, y: 402 * properties.scale, scale: properties.scale, pieceName: properties.pieceName});
-		this.addChild(this.piece4, this.piece3, this.piece2, this.piece1);
-		this.set({x: properties.x, y: properties.y});
+		if(properties.numberOfPieces == 3){
+			this.addChild(this.piece4, this.piece3, this.piece2, this.piece1 );
+			this.set({x: properties.x, y: properties.y});
+		};
+		if(properties.numberOfPieces == 4){
+			this.addChild(this.piece3, this.piece2, this.piece1);
+			this.set({x: properties.x, y: properties.y}); 
+		};
+
+		//this.addChild(this.piece4, this.piece3, this.piece2, this.piece1);
+		//this.set({x: properties.x, y: properties.y});
 	};
 	Column.prototype = new createjs.Container();
 	Column.prototype.Container_initialize = Column.prototype.initialize;
